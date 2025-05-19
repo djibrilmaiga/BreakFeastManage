9e1ea25b6a268993358355056f86664ae10a2bfc
Hamza SANMO <hamzasanmo@gmail.com>
2025-05-19 17:34:01 +0000

    first commit

---
+++ breakfeastmanage/bin/breakfeastmanage.dart
@@ -0,0 +1,5 @@
+import 'package:breakfeastmanage/breakfeastmanage.dart' as breakfeastmanage;
+
+void main(List<String> arguments) {
+  print('Hello world: ${breakfeastmanage.calculate()}!');
+}
---
+++ breakfeastmanage/test/breakfeastmanage_test.dart
@@ -0,0 +1,8 @@
+import 'package:breakfeastmanage/breakfeastmanage.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
