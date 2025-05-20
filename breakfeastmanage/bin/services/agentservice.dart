12378534c79ebdf8edd46ba11ec7fd566caaa9be
HamzaSANMO <hamzasanmo@gmail.com>
2025-05-20 08:49:19 +0000

    tueday morning

--- breakfeastmanage/bin/services/agentservice.dart
+++ breakfeastmanage/bin/services/agentservice.dart
@@ -6,8 +6,8 @@ import '../models/agent.dart';
 class Agentservice {
   // methode pour faire l'authentification d'un agent
 
-  String? authentification (String Email, String mot_de_passe){
-
+  String? authentification (String Email, String mot_de_passe) async{
+  File file = File("usersdata.json");
 
   // prendre les entrer de l'utilisateur
   print("Veillez entrer votre Email:");
@@ -15,32 +15,25 @@ class Agentservice {
   print("Veiller saisir votre mot de passe:");
   String? motDePasse = stdin.readLineSync();
 
+  List <dynamic> agents = [];
   // faire la lecture des informations de l'agent dans le fichier json
   if (await file.exists()){
     final contenu = await file.readAsString();
     final Map <String, dynamic> dataJson = jsonDecode(contenu);
+    agents = dataJson["agents"];
 
   }
 
   // boucle pour verifier si les entrer corresponde au contenu du json
 
-  for (Agent in contenu){
-    if (emailAgent == Agent.email && motDePasse == Agent.mot_de_passe){
+  for (var agent in agents){
+    if (emailAgent == agent["email"] && motDePasse == agent["mot_de_passe"] && agent["estActif"] == true){
       print("Les informations fournies sont correctes!");
     }
     else{
       print("Echec! Veillez saisir un email et un mot de passe valide");
     }
   }
-
-  // verifier si le status est actif ou non
-  if(Agent.status == "actif"){
-    print("Bienvenue sur notre plateforme!");
-  }
-  else {
-    print("Votre compte a été désactiver veillez contacter un administrateur !");
-  }
-
     
   }
 }
